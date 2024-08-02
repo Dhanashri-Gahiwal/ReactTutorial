594f6ab68c0e67c263fd18c4ebc55a67c366a6d9
Dhanashri Gahiwal <gahiwald@gmail.com>
2024-08-02 19:17:24 +0530

    Install react app using create-react-app

---
+++ ReactUsing_create-react-app/myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Install React App Using</h1>
+      <p>npx create-react-app appName</p>
+    </div>
+  );
+}
+
+export default App;
