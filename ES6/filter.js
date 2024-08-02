098826944449fe4a2cf5d61805b03911e2e83eca
Dhanashri Gahiwal <gahiwald@gmail.com>
2024-08-02 14:45:41 +0530

    array methods

---
+++ ES6/filter.js
@@ -0,0 +1,32 @@
+let employee = [
+    {
+        ename : "Johny",
+        age : 35,
+        designation : "Software Developer",
+        salary : 50000
+    },
+    {
+        ename : "Sunny",
+        age : 30,
+        designation : "Web Developer",
+        salary : 40000
+    },
+    {
+        ename : "Danny",
+        age : 27,
+        designation : "Backend Developer",
+        salary : 45000
+    },
+    {
+        ename : "Eny",
+        age : 32,
+        designation : "Frotend Developer",
+        salary : 45000
+    }
+]
+
+let employeeDetails = employee.filter((i,j)=>{
+    return i.age > 30;
+})
+
+console.log(employeeDetails);
